134ee45e75f4b9b74aa113a40a5799e862cc68bc
Eduardo Tenório <eduardo.tenorio@creditas.com.br>
2018-11-08 21:05:07 -0200

    Changing <something>_object functions names to <something>_string.

--- aws/s3.py
+++ aws/s3.py
@@ -14,25 +14,25 @@ def get_s3_object(bucket_name, key):
     s3_resource = boto3.resource('s3')
     return s3_resource.Object(bucket_name, key)
 
-def read_object(bucket_name, key):
+def read_string(bucket_name, key):
     obj = get_s3_object(bucket_name, key)
     data = obj.get()['Body'].read()
     return data.decode('utf8')
 
-def write_object(bucket_name, key, data):
+def write_string(bucket_name, key, data):
     obj = get_s3_object(bucket_name, key)
     obj.put(Body=data)
 
 
 def read_json_lines(bucket_name, key):
-    data = read_object(bucket_name, key)
+    data = read_string(bucket_name, key)
     lines = data.split('\n')
     return [json.loads(line) for line in lines]
 
 def write_json_lines(bucket_name, key, data):
     lines = [json.dumps(obj, ensure_ascii=False) for obj in data]
     data = '\n'.join(lines)
-    write_object(bucket_name, key, data)
+    write_string(bucket_name, key, data)
 
 
 # TODO create code similar to the ones above using airflow (using dependency injection)
