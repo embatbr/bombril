adfa7ed2a570c7089eb8f07ac9634af4581a2594
Eduardo Tenório <embatbr@gmail.com>
2019-01-14 11:42:47 -0200

    Removing unused imports.

--- cryptography/rsa.py
+++ cryptography/rsa.py
@@ -4,14 +4,13 @@ import base64
 
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
-# from cryptography.hazmat.primitives.asymmetric import dsa, rsa
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives.serialization import load_pem_private_key, load_pem_public_key
 
 
 def encrypt(message, public_key_pem):
-    public_key_pem_bytes = public_key_pem.encode('utf8')
     message_bytes = message.encode('utf8')
+    public_key_pem_bytes = public_key_pem.encode('utf8')
 
     pubkey = load_pem_public_key(public_key_pem_bytes, backend=default_backend())
     encrypted_message = pubkey.encrypt(
@@ -30,8 +29,8 @@ def encrypt(message, public_key_pem):
 
 
 def decrypt(encrypted_message, private_key_pem):
-    private_key_pem_bytes = private_key_pem.encode('utf8')
     encrypted_message_bytes = encrypted_message.encode('utf8')
+    private_key_pem_bytes = private_key_pem.encode('utf8')
 
     encrypted_message_bytes = base64.b64decode(encrypted_message_bytes)
 
