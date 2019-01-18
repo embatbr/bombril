7215f106cc050f63b9228c1a1d29900535928217
Eduardo Tenório <eduardo.tenorio@creditas.com.br>
2019-01-18 13:43:18 -0200

    Logging improved.

--- bombril/logging/__init__.py
+++ bombril/logging/__init__.py
@@ -12,4 +12,46 @@ def get_logger(name, level_name='INFO'):
     level = getattr(logging, level_name, logging.NOTSET)
     logger.setLevel(level)
 
+    # TODO remove the duplication
+
+    def log_critical(self):
+        func = self.critical
+        def _internal(msg, *args):
+            func(msg.format(*args))
+        return _internal
+
+    logger.critical = log_critical
+
+    def log_error(self):
+        func = self.error
+        def _internal(msg, *args):
+            func(msg.format(*args))
+        return _internal
+
+    logger.error = log_error(logger)
+
+    def log_warning(self):
+        func = self.warning
+        def _internal(msg, *args):
+            func(msg.format(*args))
+        return _internal
+
+    logger.warning = log_warning(logger)
+
+    def log_info(self):
+        func = self.info
+        def _internal(msg, *args):
+            func(msg.format(*args))
+        return _internal
+
+    logger.info = log_info(logger)
+
+    def log_debug(self):
+        func = self.debug
+        def _internal(msg, *args):
+            func(msg.format(*args))
+        return _internal
+
+    logger.debug = log_debug(logger)
+
     return logger
